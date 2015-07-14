6c622d16d77606ea7a54addfbe8105d2385c5e1b
Alex <saniokua@meta.ua>
2015-07-14 16:08:44 +0300

    rework  CallReceiver open activity

--- app/src/main/java/azaza/myapplication/Reciver/CallReceiver.java
+++ app/src/main/java/azaza/myapplication/Reciver/CallReceiver.java
@@ -10,18 +10,13 @@ import azaza.myapplication.GlobalData.PhoneData;
 import azaza.myapplication.Libs.Contacts.GetContactInfo;
 import azaza.myapplication.Model.ContactItem;
 
-/**
- * Created by Alex on 05.06.2015.
- */
 public class CallReceiver extends PhonecallReceiver {
 
     GetContactInfo getContactInfo = new GetContactInfo();
     List<ContactItem> listContactItem = new ArrayList();
 
-
     @Override
     protected void onIncomingCallStarted(Context ctx, String number, String start) {
-
     }
 
     @Override
@@ -32,19 +27,17 @@ public class CallReceiver extends PhonecallReceiver {
     protected void onIncomingCallEnded(Context ctx, String number, String start, String end) {
 
         listContactItem = getContactInfo.getContactData(ctx, number);
-
         PhoneData.CONTACT = listContactItem.get(0).getContactName();
         PhoneData.PHONE = listContactItem.get(0).getContactNubmer();
         PhoneData.IMAGE = listContactItem.get(0).getContactImage();
 
-        PhoneData.DATE=end.toString();
+        PhoneData.DATE = end.toString();
         PhoneData.myTYPE = "0";
 
         Intent i = new Intent();
         i.setClassName("azaza.myapplication", "azaza.myapplication.CallActivity");
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         ctx.startActivity(i);
-
     }
 
     @Override
@@ -55,24 +48,17 @@ public class CallReceiver extends PhonecallReceiver {
         PhoneData.CONTACT = listContactItem.get(0).getContactName();
         PhoneData.PHONE = listContactItem.get(0).getContactNubmer();
         PhoneData.IMAGE = listContactItem.get(0).getContactImage();
-        PhoneData.DATE=end.toString();
+        PhoneData.DATE = end.toString();
         PhoneData.myTYPE = "1";
-
 
         Intent i = new Intent();
         i.setClassName("azaza.myapplication", "azaza.myapplication.CallActivity");
         i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         ctx.startActivity(i);
-
-
-
     }
 
     @Override
     protected void onMissedCall(Context ctx, String number, String start) {
     }
-
-
-
 
 }
